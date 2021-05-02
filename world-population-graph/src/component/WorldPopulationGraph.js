2ef6f4654639cbf9daefdf0fc83616648ccfcc42
Tyler Beringer <99tylerberinger@gmail.com>
2021-05-02 14:41:46 -0400

    Created component WorldPopulationGraph

---
+++ world-population-graph/src/component/WorldPopulationGraph.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class WorldPopulationGraph extends React.Component {
+
+    render() {
+        return (
+
+            <h1>World Population Graph</h1>
+
+        )
+    }
+
+}
+
+export default WorldPopulationGraph
--- world-population-graph/src/index.js
+++ world-population-graph/src/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import WorldPopulationGraph from "./component/WorldPopulationGraph"
+
 import "./index.css"
 
 ReactDOM.render(
-    <h1>World Population Graph</h1>,
+    <WorldPopulationGraph />,
     document.getElementById("root")
 )

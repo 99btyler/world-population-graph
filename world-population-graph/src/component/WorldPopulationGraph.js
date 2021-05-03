e098a0232b9fe4ac6c70e8d06809becbe3cf5ea2
Tyler Beringer <99tylerberinger@gmail.com>
2021-05-03 14:37:54 -0400

    Added component Header

--- world-population-graph/src/component/WorldPopulationGraph.js
+++ world-population-graph/src/component/WorldPopulationGraph.js
@@ -1,11 +1,13 @@
 import React from "react"
 
+import Header from "./components/Header.js"
+
 class WorldPopulationGraph extends React.Component {
 
     render() {
         return (
 
-            <h1>World Population Graph</h1>
+            <Header />
 
         )
     }
---
+++ world-population-graph/src/component/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+    return (
+
+        <h1>World Population Graph</h1>
+
+    )
+}
+
+export default Header

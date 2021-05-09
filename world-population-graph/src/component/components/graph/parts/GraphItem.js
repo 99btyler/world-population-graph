4f22876aa64c621a4033ee76b8182c739ae25c88
Tyler Beringer <99tylerberinger@gmail.com>
2021-05-09 16:45:52 -0400

    Added additional visual of country's population

--- world-population-graph/src/component/components/graph/parts/GraphItem.js
+++ world-population-graph/src/component/components/graph/parts/GraphItem.js
@@ -6,8 +6,17 @@ function GraphItem(props) {
     return (
 
         <div id="graphitem">
-            <p>{props.countryName}</p>
-            <div style={{width: (((props.countryPopulation / 1400000000) * 100) - 10) + "%"}}></div>
+
+            { /* Country's name */ }
+            <p id="graphitem-countryname">{props.countryName}</p>
+
+            <div id="graphitem-countrypopulation-bar" style={{width: (((props.countryPopulation / 1400000000) * 100) - 10) + "%"}}>
+
+                { /* Country's population */ }
+                <div id="graphitem-countrypopulation">{props.countryPopulation}</div>
+
+            </div>
+
         </div>
 
     )
